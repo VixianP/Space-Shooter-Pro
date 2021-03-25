8f7ba995fc8e8145fbffb311d3f7f8b18f5758f1
VixianP <vixian@protonmail.com>
2021-03-25 16:50:36 -0700

    Laser

---
+++ Assets/Scripts/LaserBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBehavior : MonoBehaviour
+{   
+    [SerializeField]
+    private float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(new Vector3(0, speed, 0) * Time.deltaTime);
+        if(transform.position.y > 8)
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float Speed = 5;
+    [SerializeField]
+    private GameObject LaserPrefab;
+    [SerializeField]
+    private float BulletOffSet;
+
+    [SerializeField]
+    private float CanFire = -1;
+    [SerializeField]
+    private float FireRate = 0.5f;
+
+    void Update()
+    {
+        Fire();
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        float VerticalInput = Input.GetAxis("Vertical");
+        Boundaries();
+        transform.Translate(new Vector3(HorizontalInput, VerticalInput, 0)* Speed * Time.deltaTime);
+    }
+    void Fire()
+    {
+        if (Input.GetKeyDown(KeyCode.Space ) && Time.time >= CanFire)
+        {
+            CanFire = Time.time + FireRate;
+            Instantiate(LaserPrefab, new Vector3(transform.position.x,transform.position.y + BulletOffSet, transform.position.z), Quaternion.identity);
+        }
+    }
+
+    public void Boundaries()
+    {
+    
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f,0), 0);
+
+        if (transform.position.x > 11)
+        {
+            transform.position = new Vector3(-11, transform.position.y, 0);
+        } else if (transform.position.x < -11)
+        {
+            transform.position = new Vector3(11, transform.position.y, 0);
+        }
+    }
+}
